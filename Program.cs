61e154d6c3595a712b54319218e407365ed16652
Daniel Albuquerque <106679918+dnxbatista@users.noreply.github.com>
2025-09-04 00:29:44 -0300

    Iniciando Projeto

---
+++ Program.cs
@@ -0,0 +1,18 @@
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+app.MapGet("/", () => "Hello World!");
+
+app.MapPost("/login", (minimal_api.Dominio.DTOs.LoginDTO loginDTO) =>
+{
+    if (loginDTO.Email == "adm@teste.com" && loginDTO.Senha == "123456")
+    {
+        return Results.Ok("Logado com sucesso!");
+    }
+    else
+    {
+        return Results.Unauthorized();
+    }
+});
+
+app.Run();
